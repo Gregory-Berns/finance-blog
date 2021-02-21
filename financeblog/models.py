2fd3b607c3e9d178174dafa880935afb3ed313a0
Gregory Barns <grgromondi@gmail.com>
2021-02-21 18:48:32 +0300

    Added a sub_title field to the Post db model

--- financeblog/models.py
+++ financeblog/models.py
@@ -14,15 +14,14 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')" 
-
-
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+    #sub_title = db.Column(db.String(300), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    image_file = db.Column(db.String(20), nullable=False, default='default_post_img.jpg')
+    post_image = db.Column(db.String(20), nullable=False, default='default_post_img.jpg')
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}', '{self.image_file}')" 
+        return f"Post('{self.title}', '{self.date_posted}', '{self.post_image}')" 
